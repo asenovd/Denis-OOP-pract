82bcb7ef3b1b227c861ca977a7bed7e8f6ebdb14
asenovd <62379081+asenovd@users.noreply.github.com>
2020-04-11 11:01:03 +0300

    cleaned up code

--- Practice_07/Shapes/rectangular.cpp
+++ Practice_07/Shapes/rectangular.cpp
@@ -2,18 +2,18 @@
 #include "shape.hpp"
 #include "rectangular.hpp"
 
-Rectangular::Rectangular() : Shape::Shape() {
+Rectangular::Rectangular() : Shape() {
   width = 0;
   length = 0;
 }
 
-Rectangular::Rectangular(double temp_width, double temp_length) : Shape::Shape() {
+Rectangular::Rectangular(double temp_width, double temp_length) : Shape() {
   width = temp_width;
   length = temp_length;
 }
 
 
-Rectangular::Rectangular(TYPE temp_type, unsigned temp_dim, double temp_width, double temp_length) : Shape::Shape(temp_type,temp_dim) {
+Rectangular::Rectangular(TYPE temp_type, unsigned temp_dim, double temp_width, double temp_length) : Shape(temp_type,temp_dim) {
   width = temp_width;
   length = temp_length;
 }
